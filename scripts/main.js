90ceb8e7e6c302424145b9e2c30fcd11539eb9d0
carocledc3 <carocledc3@gmail.com>
2024-12-06 22:58:08 +0800

    dark mode toggle

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 $(function() {
     $(".switch").click(function() {
         $("html").toggleClass("dark");
+        if($(".switch").html() == "light_mode") {
+            $(".switch").html("dark_mode")
+        } else {
+            $(".switch").html("light_mode")
+        }
+        ;
     });
 });
